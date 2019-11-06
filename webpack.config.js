76245cee70d2bd382eb60b8b2a2cfc3331d90d33
Kristijan Fištrek <kristijan.fistrek@t.ht.hr>
2019-11-06 09:44:24 +0100

    #18 - Add: Source map added as dev tools for debbuging

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     entry: {
         app: './src/index.js',
         print: './src/print.js',
     },
+    devtool: 'inline-source-map',
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
